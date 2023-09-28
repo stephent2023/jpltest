ae02d7c1903eb135d4535a7ce14278131083b49d
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2023-09-28 16:00:04 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,1 @@
-echo '# Use Python 3.6 or later as a base image
-FROM python:3.9
-# Copy contents into image
-COPY . .
-# Install pip dependencies from requirements
-RUN pip install -r requirements.txt
-# Set YOUR_NAME environment variable
-ENV YOUR_NAME stephen
-# Expose the correct port
-EXPOSE 5500
-# Create an entrypoint
-ENTRYPoint ["python","app.py"]' > 'dockerfileexercise/Task1/Dockerfile'
+
