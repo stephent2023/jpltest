2f170580749cb1079e1b5950e7a3d9c3936f670f
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2023-09-28 15:57:04 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,3 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 git clone https://gitlab.com/Reece-Elder/dockerfileexercise.git || true
-cd dockerfileexercise/Task1
