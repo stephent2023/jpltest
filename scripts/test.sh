c87b6bd0b8c547bdb087a21e5ec12b62e6b3a6a4
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2023-09-29 11:02:14 +0100

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,3 +6,21 @@ RUN npm install
 COPY . .
 EXPOSE 5500
 ENTRYPoint ["npm","start"]' > 'devops-m5-nodeproject/Dockerfile'
+
+echo '
+{
+  "name": "nodejs-project",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.18.1"
+  }
+}
+' > devops-m5-nodeproject/package.json
