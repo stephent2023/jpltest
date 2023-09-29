647feabd78c5850e7dc87ae98f17483c1896daf8
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2023-09-29 10:54:40 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
-docker build -t epic:${BUILD_NUMBER} /devops-m5-nodeproject/
+docker build -t epic:${BUILD_NUMBER} devops-m5-nodeproject
 docker run -d -p 80:5000 --name Container epic:${BUILD_NUMBER}
