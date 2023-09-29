b50d31fc6c6a77a21ff736f368742afac47b70f4
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2023-09-29 10:43:42 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
-git clone https://gitlab.com/Reece-Elder/dockerfileexercise.git || true
+git clone https://gitlab.com/Reece-Elder/devops-m5-nodeproject || true
