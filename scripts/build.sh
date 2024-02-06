4f22601f11a7d87d6d86b544b049989bceb8a6dc
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2024-02-06 11:59:03 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
-git clone -b main #### || true
+git clone -b main https://github.com/stephent2023/hgpipeline || true
 docker build -t hugorunner:${BUILD_NUMBER} Hugo
 docker run -d -p 8501:8501 --name Container hugorunner:${BUILD_NUMBER}
